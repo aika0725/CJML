414549ec9a01a7d8cb5a3d5d2957076e4ef0dbd8
aika0725 <32117865+aika0725@users.noreply.github.com>
2022-01-14 19:25:03 +0100

    add docs

--- function/mask.js
+++ function/mask.js
@@ -3,3 +3,15 @@ const removeMask = ()=>{
     mask.classList.remove('show')
     setTimeout(() => {mask.remove()}, 500);
 }
+
+
+const readDocs=()=>{
+    removeMask()
+    window.open("docs/doc.html", "_blank");
+}
+
+const readMore=()=>{
+    removeMask()
+    window.open("https://www.sintef.no/en/expertise/information-and-communication-technology-ict/software-service-and-innovation/user-journeys-and-customer-journey-modeling-language-cjml/", "_blank");
+}
+
