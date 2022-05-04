dd43df7d9f49058d7d5aa2adb0a3c55b6072493b
André Santos <41214812+andredevsantos@users.noreply.github.com>
2022-05-04 21:50:12 +0100

    Fix png export

--- function/exportPng.js
+++ function/exportPng.js
@@ -1,17 +1,21 @@
-const exportPng = ()=>{
-    html2canvas(document.querySelector("#capture")).then(canvas => {
-        exportCanvasAsPNG(canvas) 
-    })
-}
+const exportPng = () => {
+    html2canvas(document.querySelector("#capture")).then((canvas) => {
+        exportCanvasAsPNG(canvas);
+    });
+};
 
-const exportCanvasAsPNG = (canvas)=>{
-    const imgURL = canvas.toDataURL("image/png")
-    let dlLink = document.createElement('a');
-    dlLink.download = 'CJML';
+const exportCanvasAsPNG = (canvas) => {
+    const imgURL = canvas.toDataURL("image/png");
+    let dlLink = document.createElement("a");
+    dlLink.download = "CJML";
     dlLink.href = imgURL;
-    dlLink.dataset.downloadurl = ["image/png", dlLink.download, dlLink.href].join(':');
+    dlLink.dataset.downloadurl = [
+        "image/png",
+        dlLink.download,
+        dlLink.href,
+    ].join(":");
 
     document.body.appendChild(dlLink);
     dlLink.click();
     document.body.removeChild(dlLink);
-}
+};
