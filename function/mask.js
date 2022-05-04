556dc263d55749622caecede88e3b0f0703a7988
André Santos <41214812+andredevsantos@users.noreply.github.com>
2022-05-04 21:57:54 +0100

    Update mask.js

--- function/mask.js
+++ function/mask.js
@@ -1,17 +1,17 @@
-const removeMask = ()=>{
-    const mask = document.querySelector('.modal-backdrop')
-    mask.classList.remove('show')
-    setTimeout(() => {mask.remove()}, 500);
-}
+const removeMask = () => {
+    const mask = document.querySelector(".modal-backdrop");
+    mask.classList.remove("show");
+    setTimeout(() => {
+        mask.remove();
+    }, 500);
+};
 
-
-const readDocs=()=>{
-    removeMask()
+const readDocs = () => {
+    removeMask();
     window.open("docs/doc.html", "_blank");
-}
+};
 
-const readMore=()=>{
-    removeMask()
+const readMore = () => {
+    removeMask();
     window.open("https://www.cjml.no", "_blank");
-}
-
+};
